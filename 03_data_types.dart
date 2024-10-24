121cc11b5f138c0c4485c220e7e78f961dd4b740
Damoiskii <moimyazz@gmail.com>
2024-10-24 11:14:40 -0500

    Added data types file.

---
+++ 03_data_types.dart
@@ -0,0 +1,29 @@
+void main() {
+  // Data Types
+  // Numbers, Strings, Booleans, Lists, Maps, Sets, Dynamic
+
+  // Strings
+  String firstName = "Moi";
+  String lastName = "My";
+  print("Firstname: " + firstName);
+  print("Lastname: $lastName");
+
+  // Integers
+  int age = 30;
+  print("Age: $age");
+
+  // Doubles
+  double gpa = 3.66;
+  print("GPA: $gpa");
+
+  // Booleans
+  bool found = false;
+  print("Found: $found");
+
+  // Dynamic
+  dynamic text = "Hello world!";
+  print("Dynamic: $text");
+
+  text = 10;
+  print("Dynamic [after update]: $text");
+}
