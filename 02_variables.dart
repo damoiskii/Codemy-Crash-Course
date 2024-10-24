da289ea1b2a74d43604443cee34f5db07ac45788
Damoiskii <moimyazz@gmail.com>
2024-10-24 11:01:50 -0500

    first commit

---
+++ 02_variables.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: unused_local_variable
+
+void main() {
+  // Strings
+  String name = "John Tom";
+
+  //Integer
+  int age = 30;
+
+  // Double
+  double height = 1.8;
+
+  // Boolean
+  bool isStudent = true;
+
+  // var -> This is any value ('Hello', 10, etc.) is enforcing that value to the variable without have to specify a datatype.
+  var x = "Butter";
+
+  // Dynamic
+  dynamic id = "1000";
+  id = 1000; // Dynamically changing the type
+
+  // Const and Final
+  const String fullName = "Shelly Price";
+  final String lastName = "Fisher";
+
+  // Null values
+  var y;
+  var z = null;
+
+  // Char
+  // char grade = 'A'; // char doesn't work in dart
+
+  // Float
+  // float weight = 75.5f; float doesn't work in dart
+}
